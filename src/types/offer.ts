0a9e2d9608841bb5b42c74191341613561b27ba2
Tatyan <23angorka07@gmail.com>
2023-10-27 10:30:26 +0300

    feat: add types and mocks

---
+++ src/types/offer.ts
@@ -0,0 +1,34 @@
+export type Offer = {
+  bedrooms: number;
+  city: {
+    location: Location;
+    name: string;
+  };
+  description: string;
+  goods: string[];
+  host: Host;
+  id: number;
+  images: string[];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: Location;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+}
+
+export type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export type Host = {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+}

d1f7d719d765cf968ec486d92356766f63cb4276
Tatyana Nemchina <23angorka07@gmail.com>
2023-11-30 20:46:24 +0300

    fix: changed header component

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,5 @@
+import { AuthorizationStatus } from '../const';
+
 function capitalizeFirstLetter (word: string) {
   return word.slice(0, 1).toUpperCase() + word.slice(1);
 }
@@ -6,4 +8,8 @@ function addPluralEnding (itemsCount: number) {
   return itemsCount > 1 ? 's' : '';
 }
 
-export {capitalizeFirstLetter, addPluralEnding};
+function checkAuthorizationStatus(status: AuthorizationStatus) {
+  return status === AuthorizationStatus.Auth;
+}
+
+export {capitalizeFirstLetter, addPluralEnding, checkAuthorizationStatus};
